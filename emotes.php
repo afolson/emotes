e43d1d2c701352f0a739b03e025330566a9a5f8a
Amanda Folson <amanda@incredibl.org>
2016-02-06 11:47:15 -0500

    Initial import of crap.

---
+++ emotes.php
@@ -0,0 +1,28 @@
+<?php
+/*$sadfaces = array(':(', ':<', 'D:', ':\'(', ';-;');
+print_r($sadfaces);
+
+$happyfaces = array(':)', ':D', '^_^', ':3', ':>');
+print_r($happyfaces);
+
+$shockfaces = array(':o', 'O_O', 'D:', 'o.O', 'O.o', ':/');
+print_r($shockfaces);
+
+$angryfaces = array('>:/', '>:D', '>:S', '>:O', '>:(', '>:3', '-.-');
+print_r($angryfaces);*/
+
+			//$happyFaces = array(":)", ":D", "^_^", ":3", ":>","(^-^*)");
+			/*$happyFaces = array("sdfhsfgh", "retjngsdf", "sdfgsdfstdj", "sadfasfg", "weqwe","4");
+			$randomFace = array_rand($happyFaces, 1);
+			echo $happyFaces[$randomFace[0]]. "lol";
+$input = array("Neo", "Morpheus", "Trinity", "Cypher", "Tank");
+$rand_keys = array_rand($input, 2);
+echo $input[$rand_keys[0]] . "\n";
+echo $input[$rand_keys[1]] . "\n";*/
+
+$input = array(":)", ":D", "^_^", ":3", ":>","(^-^*)", "poop");
+$rand_keys = array_rand($input, 2);
+echo $input[$rand_keys[0]] . "\n";
+//echo $input[$rand_keys[1]] . "\n";
+
+?>
